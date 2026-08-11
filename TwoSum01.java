4da2c91fc4eaec6f7388249f6345d78f3e901043
kaushikganesh <kaushikganesh1512@gmail.com>
2026-08-11 11:34:41 +0530

    Update TwoSum01.java

--- TwoSum01.java
+++ TwoSum01.java
@@ -1,12 +1,9 @@
 Problem:
-
 Given an array of integers nums and an integer target, return the indices of the two numbers such that they add up to target.
 You may assume that each input has exactly one solution.
 
 Example:
-
 Input:  nums = [2, 7, 11, 15], target = 9
-
 Output: [0, 1]
 
 Explanation:
