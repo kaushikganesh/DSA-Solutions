92dbd6d078fbf527316293d1c88ee8531192ee06
kaushikganesh <kaushikganesh1512@gmail.com>
2026-08-11 11:17:02 +0530

    Update TwoSum01.java

--- TwoSum01.java
+++ TwoSum01.java
@@ -1,3 +1,20 @@
+Problem:
+
+Given an array of integers nums and an integer target, return the indices of the two numbers such that they add up to target.
+You may assume that each input has exactly one solution.
+
+Example:
+
+Input:  nums = [2, 7, 11, 15], target = 9
+
+Output: [0, 1]
+
+Explanation:
+nums[0] + nums[1] = 2 + 7 = 9
+
+
+Code:
+
 import java.util.HashMap;
 import java.util.Map;
 
